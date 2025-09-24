d85a4a5331785799e5de591a5339e97cdefe7c9d
carlosreyes <carlos.adonai.reyes@gmail.com>
2025-09-23 19:33:36 -0600

    agregue nueva configuracion

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -1,26 +1,26 @@
 
-import os
+#import os
 
 ## set public interface
-c.JupyterHub.ip = os.environ["JUPYTERHUB_IP"]
-c.JupyterHub.port = int(os.environ["JUPYTERHUB_PORT"])
-c.JupyterHub.hub_connect_ip = os.environ["JUPYTERHUB_IP"]
+#c.JupyterHub.ip = os.environ["JUPYTERHUB_IP"]
+#c.JupyterHub.port = int(os.environ["JUPYTERHUB_PORT"])
+#c.JupyterHub.hub_connect_ip = os.environ["JUPYTERHUB_IP"]
 
 ## jupyterhub auth settings
-admin_users = os.environ['ADMIN_USERS']
-print("Admin Users", admin_users)
-admin_users_set = set(filter(len, map(str.strip, admin_users.split(','))))
-print("Admin Users Set", admin_users_set)
+# admin_users = os.environ['ADMIN_USERS']
+# print("Admin Users", admin_users)
+# admin_users_set = set(filter(len, map(str.strip, admin_users.split(','))))
+# print("Admin Users Set", admin_users_set)
 
-c.JupyterHub.authenticator_class = "oauthenticator.LocalGitHubOAuthenticator"
-c.LocalGitHubOAuthenticator.create_system_users = True
-c.LocalGitHubOAuthenticator.allowed_users = admin_users_set
-c.Authenticator.admin_users = admin_users_set
+#c.JupyterHub.authenticator_class = "oauthenticator.LocalGitHubOAuthenticator"
+#c.LocalGitHubOAuthenticator.create_system_users = True
+#c.LocalGitHubOAuthenticator.allowed_users = admin_users_set
+#c.Authenticator.admin_users = admin_users_set
 #c.JupyterHub.admin_access = True
 
 ## mount a data location to persist login and user data
-data_dir = os.environ['JUPYTERHUB_DATA']
-c.JupyterHub.cookie_secret_file = os.path.join(data_dir, 'jupyterhub_cookie_secret')
+#data_dir = os.environ['JUPYTERHUB_DATA']
+#c.JupyterHub.cookie_secret_file = os.path.join(data_dir, 'jupyterhub_cookie_secret')
 
 ## remove proxy file as it gives error if it exists on container restart
 #proxy_pid_file = "/var/run/jupyterhub-proxy.pid"
@@ -28,3 +28,28 @@ c.JupyterHub.cookie_secret_file = os.path.join(data_dir, 'jupyterhub_cookie_secr
 #    print("Cleaning up proxy pid file")
 #    os.remove(proxy_pid_file)
 #c.ConfigurableHTTPProxy.pid_file = proxy_pid_file
+
+
+
+import os
+
+c = get_config()
+c.JupyterHub.ip = '127.0.0.1'
+c.JupyterHub.port = int(os.environ["JUPYTERHUB_PORT_INTERNAL"])
+c.JupyterHub.hub_connect_ip = 'jupyterhub'
+
+admin_users = os.environ['ADMIN_USERS']
+print("Admin Users", admin_users)
+admin_users_set = set(filter(len, map(str.strip, admin_users.split(','))))
+print("Admin Users Set", admin_users_set)
+
+c.JupyterHub.authenticator_class = "oauthenticator.github.LocalGitHubOAuthenticator"
+c.LocalGitHubOAuthenticator.create_system_users = True
+c.LocalGitHubOAuthenticator.allowed_users = admin_users_set
+c.Authenticator.admin_users = admin_users_set
+c.LocalGitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
+c.LocalGitHubOAuthenticator.oauth_client_id = os.environ['OAUTH_CLIENT_ID']
+c.LocalGitHubOAuthenticator.oauth_client_secret = os.environ['OAUTH_CLIENT_SECRET']
+
+data_dir = os.environ['JUPYTERHUB_DATA']
+c.JupyterHub.cookie_secret_file = os.path.join(data_dir, 'jupyterhub_cookie_secret')
